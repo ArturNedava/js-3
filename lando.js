0a86f3f027f32a22a6140af90a157dbd5159f69c
ArturNedava <121451856+ArturNedava@users.noreply.github.com>
2023-07-24 19:07:42 +0200

    First commit

---
+++ lando.js
@@ -0,0 +1,44 @@
+//let result = 5+5+"5";
+
+//console.log(typeof result);
+
+//console.log(result);
+
+
+//let email = "nedavaartur1@gmail.com";
+
+//if (email.includes('@')) {
+  //  console.log("Email верный");
+//} else {
+  //  console.log("Email недействительный.");
+//}
+
+//let totalCharacters = email.length;
+
+//console.log("всего символов в email:", totalCharacters);
+
+
+
+//let word1 = "My"
+
+//let word2 = "name"
+
+//et word3 = "is";
+
+//let fullName = word1 + " " + word2 + " " + word3 + " Viktor";
+
+//console.log(fullName);
+
+
+//let userName = "Artur"
+
+//let payment = "300"
+
+//let message = `Дякуємо, ${userName}! До сплати ${payment} гривень`;
+
+//alert(message);
+
+
+
+
+
